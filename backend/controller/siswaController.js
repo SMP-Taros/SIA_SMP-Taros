9ef1ae338eb986ac2414d2b5c1eeb7d6b5daf705
lzamzamm <notlucy78@gmail.com>
2024-01-04 20:02:40 +0700

    create siswa API

--- backend/controller/siswaController.js
+++ backend/controller/siswaController.js
@@ -65,7 +65,10 @@ const createSiswa = asyncHandler(async (req, res) => {
 
     const siswa = await Siswa.create(newSiswa);
 
-    return res.status(201).send(siswa);
+    return res.status(201).json({
+      message: "siswa berhasil ditambahkan",
+      data: siswa,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
@@ -76,6 +79,33 @@ const getSiswa = asyncHandler(async (req, res) => {
   try {
     const siswa = await Siswa.find({});
 
+    const formattedData = siswa.map((val) => {
+      return {
+        nama: val.nama,
+        nis: val.nis,
+        nisn: val.nisn,
+      };
+    });
+
+    return res.status(200).json({
+      count: siswa.length,
+      data: formattedData,
+    });
+  } catch (error) {
+    console.log(error.massage);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+const getdetailSiswa = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const siswa = await Siswa.findById(id);
+
+    if (!siswa) {
+      return res.status(401).send("siswa tidak ditemukan");
+    }
+
     return res.status(200).json({
       count: siswa.length,
       data: siswa,
@@ -85,8 +115,74 @@ const getSiswa = asyncHandler(async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 });
-const getdetailSiswa = asyncHandler(async (req, res) => {});
-const updateSiswa = asyncHandler(async (req, res) => {});
-const deleteSiswa = asyncHandler(async (req, res) => {});
+const updateSiswa = asyncHandler(async (req, res) => {
+  try {
+    let {
+      nama,
+      nis,
+      jenis_kelamin,
+      nisn,
+      tempat_lahir,
+      tanggal_lahir,
+      nik,
+      alamat,
+      email,
+      nama_ayah,
+      pekerjaan_ayah,
+      nama_ibu,
+      pekerjaan_ibu,
+    } = req.body;
+
+    if (
+      !nama &&
+      !nis &&
+      !jenis_kelamin &&
+      !nisn &&
+      !tempat_lahir &&
+      !tanggal_lahir &&
+      !nik &&
+      !alamat &&
+      !email &&
+      !nama_ayah &&
+      !pekerjaan_ayah &&
+      !nama_ibu &&
+      !pekerjaan_ibu
+    ) {
+      // console.log(request.body.nama)
+      return res.status(400).send("none of your fields have been filled");
+    }
+
+    const { id } = req.params;
+    const siswa = await Siswa.findByIdAndUpdate(id, req.body);
+
+    if (!siswa) {
+      return res.status(400).json({ message: "Siswa not found" });
+    }
+
+    return res.status(201).json({
+      message: "siswa berhasil di update",
+      data: siswa,
+    });
+  } catch (error) {
+    console.log(error.massage);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+const deleteSiswa = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const siswa = await Siswa.findByIdAndDelete(id, req.body);
+
+    if (!siswa) {
+      return res.status(401).json({ message: "Siswa not found" });
+    }
+
+    return res.status(201).send("Siswa deleted");
+  } catch (error) {
+    console.log(error.massage);
+    res.status(500).send({ message: error.message });
+  }
+});
 
 export { createSiswa, getSiswa, getdetailSiswa, updateSiswa, deleteSiswa };
--- backend/routes/siswaRoutes.js
+++ backend/routes/siswaRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
-import Siswa from "../models/siswaModel.js";
-import { createSiswa, getSiswa } from "../controller/siswaController.js";
+import {
+  createSiswa,
+  deleteSiswa,
+  getSiswa,
+  getdetailSiswa,
+  updateSiswa,
+} from "../controller/siswaController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -10,58 +15,10 @@ router.post("/", protect, createSiswa);
 //get siswa
 router.get("/", protect, getSiswa);
 
-router.get("/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-    const siswa = await Siswa.findById(id);
+router.get("/:id", protect, getdetailSiswa);
 
-    return response.status(200).json({
-      count: siswa.length,
-      data: siswa,
-    });
-  } catch (error) {
-    console.log(error.massage);
-    response.status(500).send({ message: error.message });
-  }
-});
+router.put("/:id", protect, updateSiswa);
 
-router.put("/:id", async (request, response) => {
-  try {
-    if (!request.body.nis || !request.body.calon_id || !request.body.nama) {
-      // console.log(request.body.nama)
-      return response.status(400).send({
-        message: "Send all required fields",
-      });
-    }
-
-    const { id } = request.params;
-    const siswa = await Siswa.findByIdAndUpdate(id, request.body);
-
-    if (!siswa) {
-      return response.status(400).json({ message: "Siswa not found" });
-    }
-
-    return response.status(201).send("Siswa updated");
-  } catch (error) {
-    console.log(error.massage);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-router.delete("/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-    const siswa = await Siswa.findByIdAndDelete(id, request.body);
-
-    if (!siswa) {
-      return response.status(400).json({ message: "Siswa not found" });
-    }
-
-    return response.status(201).send("Siswa deleted");
-  } catch (error) {
-    console.log(error.massage);
-    response.status(500).send({ message: error.message });
-  }
-});
+router.delete("/:id", protect, deleteSiswa);
 
 export default router;
