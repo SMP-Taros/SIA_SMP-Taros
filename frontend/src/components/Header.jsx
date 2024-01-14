2c05a8b92db9ea4557f626d3c7605370d2a6aab9
lzamzamm <notlucy78@gmail.com>
2024-01-15 00:56:53 +0700

    merge branch

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../Theme";
+
+const Header = ({ title }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ m: "0 0 5px 0" }}
+      >
+        {title}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
--- frontend/src/scanes/Dashboard/Dashboard.jsx
+++ frontend/src/scanes/Dashboard/Dashboard.jsx
@@ -1,11 +1,17 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 import Template from "../Template/TemplateScreen";
 
 const Dashboard = () => {
-    return (
-        <Template>
-            <h1>Dashboard</h1>
-        </Template>
-    )
-}
+  return (
+    <Template>
+      <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="Dashboard" />
+        </Box>
+      </Box>
+    </Template>
+  );
+};
 
 export default Dashboard;
